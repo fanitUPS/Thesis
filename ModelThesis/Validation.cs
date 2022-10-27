9cc718b0af33a3dd10f364bec48df18fa104d4ae
Фанит Загитов <fanit666@gmail.com>
2022-10-27 19:01:50 +0700

    Добалвены классы запроса и валидации данных

---
+++ ModelThesis/Validation.cs
@@ -0,0 +1,69 @@
+﻿using Ck = Monitel.Rtdb.Api;
+using System;
+using System.Collections.Generic;
+
+namespace ModelThesis
+{
+    /// <summary>
+    /// Класс для валидации данных из СК-11
+    /// </summary>
+    public class Validation
+    {
+        /// <summary>
+        /// Достоверный код качества ТИ
+        /// </summary>
+        private const string _goodQuality = "10000002";
+
+        /// <summary>
+        /// Входные данные
+        /// </summary>
+        private Ck.RtdbValue[] _inputData;
+
+        /// <summary>
+        /// Входные данные
+        /// </summary>
+        public Ck.RtdbValue[] InputData
+        {
+            get => _inputData;
+            set
+            {
+                if (value.Length == 0)
+                {
+                    throw new ArgumentException
+                        ("Массив не содержит данных.");
+                }
+
+                _inputData = value;
+            }
+        }
+
+        /// <summary>
+        /// Конструктор класса
+        /// </summary>
+        /// <param name="inputData">Входные данные для проверки</param>
+        public Validation(Ck.RtdbValue[] inputData)
+        {
+            InputData = inputData;
+        }
+
+        /// <summary>
+        /// Осуществление проверки данных
+        /// </summary>
+        /// <returns>Массив достоверных значений</returns>
+        public Ck.RtdbValue[] GetValidData()
+        {
+            var validDataList = new List<Ck.RtdbValue>();
+
+            for (int i = 0; i < this.InputData.Length; i++)
+            {
+                if (Convert.ToString
+                    (this.InputData[i].QualityCodes, 16) == _goodQuality)
+                {
+                    validDataList.Add(this.InputData[i]);
+                }
+            }
+
+            return validDataList.ToArray();
+        }
+    }
+}
--- Thesis/Program.cs
+++ Thesis/Program.cs
@@ -12,37 +12,33 @@ namespace ConsoleApp
         {
             var signalGuid1 = new Guid("65CCCE53-5914-49E7-9623-BB914962C46A");
             var signalGuid2 = new Guid("7443900b-9a2a-436d-a284-cc9400bb75dd");
+            //good 10000002
             var uidsArray = new Guid[] { signalGuid1, signalGuid2 };
 
-            var signalsData = new DataRe
+            var signalsData = new DataRequest(_connectionStringToRtdb);
+
             try
             {
+                var rtdbValues = signalsData.GetSignals(uidsArray);
+                var validationSignal = new Validation(rtdbValues);
+                var validSignals = validationSignal.GetValidData();
 
+                foreach (Ck.RtdbValue value in validSignals)
+                {
+                    Console.WriteLine("Value: " + value.Value.AnalogValue);
+                    Console.WriteLine("Type: " + value.Type);
+                    Console.WriteLine("Uid: " + value.Uid);
+                    Console.WriteLine("QualityCodes: " + Convert.ToString(value.QualityCodes, 16));
+                    Console.WriteLine("Time: " + value.Time);
+                }
+                Console.ReadKey();
             }
-
-            foreach (Ck.RtdbValue value in response.Values)
+            catch (Exception e)
             {
-                Console.WriteLine("Value: " + value.Value.AnalogValue);
-                Console.WriteLine("Type: " + value.Type);
-                Console.WriteLine("Uid: " + value.Uid);
-                Console.WriteLine("QualityCodes: " + Convert.ToString(value.QualityCodes, 16));
-                Console.WriteLine("Time: " + value.Time);
-            }
-            Console.ReadKey();
-
-
-
-
-        }
-                    catch (Ck.RequestException e)
-                    {
-                        Console.WriteLine(e.Message);
-                    }
-}
-
-                
-
+                Console.WriteLine(e.Message);
+                Console.ReadKey();
             }
         }
     }
 }
+
