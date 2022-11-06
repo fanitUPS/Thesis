53d1cf7c722a168963ecec13eb5738ba1c1c5264
Фанит Загитов <fanit666@gmail.com>
2022-11-06 22:51:25 +0700

    объеденил методы преподготовки данных

--- ModelThesis/PreparingData.cs
+++ ModelThesis/PreparingData.cs
@@ -1,18 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using ModelThesis;
+﻿using System.Collections.Generic;
 using PandasNet;
 
 namespace ModelThesis
 {
+    /// <summary>
+    /// Класс подготовки данных к расчету
+    /// </summary>
     public class PreparingData
     {
+        /// <summary>
+        /// Массив ТИ активной мощности
+        /// </summary>
         public SignalPower[] PowerSignals { get; set; }
 
+        /// <summary>
+        /// Массив ТИ тока
+        /// </summary>
         public SignalCurrent[] CurrentSignals { get; set; }
 
+        /// <summary>
+        /// Массив ТИ напряжения
+        /// </summary>
         public SignalVoltage[] VoltageSignals { get; set; }
 
+        /// <summary>
+        /// Конструктор класса
+        /// </summary>
+        /// <param name="signalPower">Массив ТИ активной мощности</param>
+        /// <param name="signalVoltage">Массив ТИ напряжения</param>
+        /// <param name="signalCurrent">Массив ТИ тока</param>
         public PreparingData(SignalPower[] signalPower,
             SignalVoltage[] signalVoltage, SignalCurrent[] signalCurrent)
         {
@@ -21,6 +37,10 @@ namespace ModelThesis
             CurrentSignals = signalCurrent;
         }
 
+        /// <summary>
+        /// Преобразование данных напряжения
+        /// </summary>
+        /// <returns>Датафрейм напряжений</returns>
         public DataFrame PreparingNodeData()
         {
             var valueList = new List<double>();
@@ -38,44 +58,65 @@ namespace ModelThesis
             var seriesMaxValue = new Series(maxValueList.ToArray());
             var seriesMinValue = new Series(minValueList.ToArray());
 
+            var columnValue = new Column();
+            columnValue.Name = "Value";
+            var columnMaxValue = new Column();
+            columnMaxValue.Name = "MaxValue";
+            var columnMinValue = new Column();
+            columnMinValue.Name = "MinValue";
+
+            var columnName = new List<Column>()
+            { columnValue,
+              columnMaxValue,
+              columnMinValue
+            };
+            
             return new DataFrame(new List<Series>() 
-                { seriesValue, seriesMaxValue, seriesMinValue });
+                { seriesValue, seriesMaxValue, seriesMinValue }, null, columnName);
         }
 
-        public DataFrame PreparingBranchCurrentData()
+        /// <summary>
+        /// Преобразование данных тока и мощности
+        /// </summary>
+        /// <returns>Датафрейм токов</returns>
+        public DataFrame PreparingBranchData(string name)
         {
             var valueList = new List<double>();
             var maxValueList = new List<double>();
 
-            foreach (var signal in CurrentSignals)
+            switch (name)
             {
-                valueList.Add(signal.SignalValue);
-                maxValueList.Add(signal.MaxCurrent);
-            }
+                case nameof(CurrentSignals):
+                    foreach (var signal in CurrentSignals)
+                    {
+                        valueList.Add(signal.SignalValue);
+                        maxValueList.Add(signal.MaxCurrent);
+                    }
+                    break;
+                case nameof(PowerSignals):
+                    foreach (var signal in PowerSignals)
+                    {
+                        valueList.Add(signal.SignalValue);
+                        maxValueList.Add(signal.MaxPower);
+                    }
+                    break;
+            };
 
             var seriesValue = new Series(valueList.ToArray());
             var seriesMaxValue = new Series(maxValueList.ToArray());
 
-            return new DataFrame(new List<Series>()
-                { seriesValue, seriesMaxValue});
-        }
+            var columnValue = new Column();
+            columnValue.Name = "Value";
+            var columnMaxValue = new Column();
+            columnMaxValue.Name = "MaxValue";
 
-        public DataFrame PreparingBranchPowerData()
-        {
-            var valueList = new List<double>();
-            var maxValueList = new List<double>();
-
-            foreach (var signal in PowerSignals)
-            {
-                valueList.Add(signal.SignalValue);
-                maxValueList.Add(signal.MaxPower);
-            }
-
-            var seriesValue = new Series(valueList.ToArray());
-            var seriesMaxValue = new Series(maxValueList.ToArray());
+            var columnName = new List<Column>()
+            { columnValue,
+              columnMaxValue
+            };
 
             return new DataFrame(new List<Series>()
-                { seriesValue, seriesMaxValue});
+                { seriesValue, seriesMaxValue}, null, columnName);
         }
     }
 }
--- Thesis/Program.cs
+++ Thesis/Program.cs
@@ -1,43 +1,55 @@
 ﻿using System;
-using Ck = Monitel.Rtdb.Api;
 using ModelThesis;
 using System.Collections.Generic;
 
-
 namespace ConsoleApp
 {
+    /// <summary>
+    /// Разрабатываемое ПО сервис для внешней Системы СК-11. Данный View создан для тестирования бизнес-логики
+    /// </summary>
     internal class Program
     {
+        /// <summary>
+        /// Строка подключения к СК-11, для запроса данных
+        /// </summary>
         private const string _connectionStringToRtdb = "10.221.3.29:900";
 
         static void Main(string[] args)
         {
+            //Гуиды созданных в системе телеизмерений
             var signalGuid1 = new Guid("65CCCE53-5914-49E7-9623-BB914962C46A");
             var signalGuid2 = new Guid("7443900b-9a2a-436d-a284-cc9400bb75dd");
-            
-            var uidsArray = new Guid[] { signalGuid1};
-
+            //Запрос данных осуществляется через массив гуидов
+            var uidsArray = new Guid[] { signalGuid1, signalGuid2 };
+            //Создаем экземпляр класса, для запроса данных
             var dataRequest = new DataRequest(_connectionStringToRtdb);
 
             try
             {
+                //Запрос данных, ответ приходит в виде массива данных
                 var dataValue = dataRequest.GetSignals(uidsArray);
                 var validationValue = new Validation(dataValue);
                 var validDataValue = validationValue.GetValidData();
-
+                //Запрос данных, ответ приходит в виде массива данных
                 var dataMaxValue = dataRequest.GetSignals(uidsArray);
                 var validationMaxValue = new Validation(dataMaxValue);
                 var validDataMaxValue = validationMaxValue.GetValidData();
-
+                //Запрос данных, ответ приходит в виде массива данных
                 var dataMinValue = dataRequest.GetSignals(uidsArray);
                 var validationMinValue = new Validation(dataMinValue);
                 var validDataMinValue = validationMinValue.GetValidData();
-
+                //Парсим данные, полученные из Системы
                 var voltage = new SignalVoltage
                     ("someName",
                     validDataValue[0].Value.AnalogValue,
                     validDataMaxValue[0].Value.AnalogValue,
                     validDataMinValue[0].Value.AnalogValue);
+
+                var voltage2 = new SignalVoltage
+                   ("someName",
+                   validDataValue[1].Value.AnalogValue,
+                   validDataMaxValue[1].Value.AnalogValue,
+                   validDataMinValue[1].Value.AnalogValue);
 
                 var power = new SignalPower
                     ("someName",
@@ -48,19 +60,27 @@ namespace ConsoleApp
                     ("someName",
                     validDataValue[0].Value.AnalogValue,
                     validDataMaxValue[0].Value.AnalogValue);
-
-                var signalVoltageArr = new SignalVoltage[] { voltage };
+                //Создадим массивы данных, для их преобразования
+                var signalVoltageArr = new SignalVoltage[] { voltage, voltage2 };
                 var signalPower = new SignalPower[] { power };
                 var signalCurrent = new SignalCurrent[] { current };
-
+                //Преобразуем данные для расчета
                 var preparingData = new PreparingData
                     (signalPower,
                     signalVoltageArr,
                     signalCurrent);
 
+                Console.WriteLine(nameof(preparingData.CurrentSignals));
+
+                var currentData = preparingData.PreparingBranchData(nameof(preparingData.CurrentSignals));
+                Console.WriteLine(currentData);
+
+                var powerData = preparingData.PreparingBranchData(nameof(preparingData.PowerSignals));
+                Console.WriteLine(powerData);
                 var voltageData = preparingData.PreparingNodeData();
 
-                Console.WriteLine(voltageData[0]);
+                Console.WriteLine(voltageData);
+
                 Console.ReadKey();
             }
             catch (Exception e)
