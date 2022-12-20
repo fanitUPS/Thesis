1e8b21ceb4032e8f5b56cfaab81a64e32b22363b
Фанит Загитов <fanit666@gmail.com>
2022-12-21 01:20:53 +0700

    last

--- Thesis/Program.cs
+++ Thesis/Program.cs
@@ -58,91 +58,97 @@ namespace ConsoleApp
             
             try
             {
-                //var starTime = DateTime.Now;
-                //Console.WriteLine(starTime);
+                Console.WriteLine("Начало работы.");
 
                 var dataBase = new DataBase(_connectionStringToDb);
-                //var dataCurrent = dataBase.SelectUuid(nameof(DataBaseTables.Currents));
-                //var dataPower = dataBase.SelectUuid(nameof(DataBaseTables.Powers));
-                //var dataVoltage = dataBase.SelectUuid(nameof(DataBaseTables.Voltages));
+                
 
-                //var voltageList = new List<SignalVoltage>();
-                //var currentList = new List<SignalCurrent>();
-                //var powerList = new List<SignalPower>();
-
-                //var time = DateTime.Now;
-
-                //foreach (var value in dataVoltage)
-                //{
-                //    var tempData = new Verification(dataRequest.GetSignals(value.Value));
-                //    var data = tempData.GetValidData();
-                //    voltageList.Add(new SignalVoltage(value.Key,
-                //        data[0].Value.AnalogValue,
-                //        (DateTime)data[0].Time,
-                //        data[1].Value.AnalogValue,
-                //        data[2].Value.AnalogValue,
-                //        data[3].Value.AnalogValue));
-                //}
-
-                //foreach (var value in dataCurrent)
-                //{
-                //    var tempData = new Verification(dataRequest.GetSignals(value.Value));
-                //    var data = tempData.GetValidData();
-                //    currentList.Add(new SignalCurrent(value.Key,
-                //        data[0].Value.AnalogValue,
-                //        (DateTime)data[0].Time,
-                //        data[1].Value.AnalogValue));
-                //}
-
-                //foreach (var value in dataPower)
-                //{
-                //    var tempData = new Verification(dataRequest.GetSignals(value.Value));
-                //    var data = tempData.GetValidData();
-                //    powerList.Add(new SignalPower(value.Key,
-                //        data[0].Value.AnalogValue,
-                //        (DateTime)data[0].Time,
-                //        data[1].Value.AnalogValue));
-                //}
-
-                //var preparingData = new PreparingData(
-                //    powerList.ToArray(),
-                //    voltageList.ToArray(),
-                //    currentList.ToArray());
-
-                //var voltages = preparingData.PreparingNodeData();
-                //var currents = preparingData.PreparingBranchData(nameof(preparingData.CurrentSignals));
-                //var powers = preparingData.PreparingBranchData(nameof(preparingData.PowerSignals));
-
-                //var calc = new Calculation(powers, currents, voltages);
-
-
-                //var index = calc.GetPerformanceIndex();
-                //Console.WriteLine(index.Value);
-                //Console.WriteLine(calc.CurrentIndex);
-                //Console.WriteLine(calc.VoltagetIndex);
-                //Console.WriteLine(calc.PowerIndex);
-
-                //var stopTime = DateTime.Now;
-                //Console.WriteLine((stopTime - starTime).TotalSeconds);
-                //Console.WriteLine(5 > (stopTime - starTime).TotalSeconds);
-                //Console.WriteLine("time of index: "+index.TimeStamp);
-
-                //var send = new DataResponse(_serverAddress, _serverPort, index, _coa, _ioa);
-                //send.SendPerformanceIndex();
-
-                //Console.WriteLine("скорость приращения:" + dataBase.GetRateOfChange(index));
-                //Console.WriteLine("Приращение: "+dataBase.GetIncrementOfIndex(index));
-
-                //dataBase.InsertData(nameof(DataBaseTables.Calculations), index);
-  
                 var uuidBranchFolder = "2F9E9FFE-755E-4BA7-85A6-0BE43626FE8A";
 
                 var model = new SynchronizeModel(_connectionStringToCK11, _nameOfModel, 55);
-                
-                var powerUuids = model.UpdatePowerUuid(uuidBranchFolder);
 
+                var powerUuids = model.UpdatePowerUuid(uuidBranchFolder);
+                Console.WriteLine("Синхронизация с ИМ.");
                 dataBase.InsertUuids(powerUuids, nameof(DataBaseTables.Powers));
-                
+                var starTime = DateTime.Now;
+                Console.WriteLine("Запрос uuid из БД Системы");
+                var dataCurrent = dataBase.SelectUuid(nameof(DataBaseTables.Currents));
+                var dataPower = dataBase.SelectUuid(nameof(DataBaseTables.Powers));
+                var dataVoltage = dataBase.SelectUuid(nameof(DataBaseTables.Voltages));
+
+                var voltageList = new List<SignalVoltage>();
+                var currentList = new List<SignalCurrent>();
+                var powerList = new List<SignalPower>();
+
+                var time = DateTime.Now;
+                Console.WriteLine("Запрос данных из БДРВ");
+                foreach (var value in dataVoltage)
+                {
+                    var tempData = new Verification(dataRequest.GetSignals(value.Value));
+                    var data = tempData.GetValidData();
+                    voltageList.Add(new SignalVoltage(value.Key,
+                        data[0].Value.AnalogValue,
+                        (DateTime)data[0].Time,
+                        data[1].Value.AnalogValue,
+                        data[2].Value.AnalogValue,
+                        data[3].Value.AnalogValue));
+                }
+                Console.WriteLine("Фильтрация данных по коду качества.");
+                foreach (var value in dataCurrent)
+                {
+                    var tempData = new Verification(dataRequest.GetSignals(value.Value));
+                    var data = tempData.GetValidData();
+                    currentList.Add(new SignalCurrent(value.Key,
+                        data[0].Value.AnalogValue,
+                        (DateTime)data[0].Time,
+                        data[1].Value.AnalogValue));
+                }
+
+                foreach (var value in dataPower)
+                {
+                    var tempData = new Verification(dataRequest.GetSignals(value.Value));
+                    var data = tempData.GetValidData();
+                    powerList.Add(new SignalPower(value.Key,
+                        data[0].Value.AnalogValue,
+                        (DateTime)data[0].Time,
+                        data[1].Value.AnalogValue));
+                }
+                Console.WriteLine("Подготовка данных к расчету.");
+                var preparingData = new PreparingData(
+                    powerList.ToArray(),
+                    voltageList.ToArray(),
+                    currentList.ToArray());
+
+                var voltages = preparingData.PreparingNodeData();
+                var currents = preparingData.PreparingBranchData(nameof(preparingData.CurrentSignals));
+                var powers = preparingData.PreparingBranchData(nameof(preparingData.PowerSignals));
+
+                var calc = new Calculation(powers, currents, voltages);
+
+                Console.WriteLine("Начало расчета");
+                var index = calc.GetPerformanceIndex();
+                Console.WriteLine("Результаты расчета расчета");
+                Console.WriteLine(index.Value);
+                Console.WriteLine(calc.CurrentIndex);
+                Console.WriteLine(calc.VoltagetIndex);
+                Console.WriteLine(calc.PowerIndex);
+
+                var stopTime = DateTime.Now;
+                Console.WriteLine("Время расчета:");
+                Console.WriteLine((stopTime - starTime).TotalSeconds);
+
+
+                Console.WriteLine("Передача результата в СК-11");
+                var send = new DataResponse(_serverAddress, _serverPort, index, _coa, _ioa);
+                send.SendPerformanceIndex();
+
+                Console.WriteLine("скорость приращения:" + dataBase.GetRateOfChange(index));
+                Console.WriteLine("Приращение: " + dataBase.GetIncrementOfIndex(index));
+
+                dataBase.InsertPerformanceIndex(nameof(DataBaseTables.Calculations), index);
+
+
+
                 Console.WriteLine("Finish");
                 Console.ReadKey();
             }
